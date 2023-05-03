dd6221a1708ccf77cb2be843b42645683d0d30a3
rasheed119 <rasheed20052000@gmail.com>
2023-05-03 13:22:28 +0530

    changes in style and phone data

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       
       <section className='py-5'>
         <div className='container px-4 px-lg-5 mt-5'>
-          <div className='row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center'>
+          <div className='card-container'>
             {phone_data.map((object,index)=>(
               <Content
               key={index}
